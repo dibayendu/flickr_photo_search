f7ac2a1967af2473ba4a0b1f9d8287f3bca058be
Dibayendu Dey <dibayendu.dey@gmail.com>
2014-08-20 22:22:15 +1000

    commenting out active record as database is not necessary

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
+# NOTE: commenting the line below as database won't be necessary
+# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
