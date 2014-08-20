a739a28382da23111d763449833f971973e47723
Dibayendu Dey <dibayendu.dey@gmail.com>
2014-08-20 22:02:22 +1000

    starting a new rails app without test framework
    will be adding rspec testing framework in forth comming commits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlickrPhotoSearch::Application.config.secret_key_base = '755372d7a44acf7691948932365998b5f8225481349accba0b772857de6f8d225f8e3c2b1ce79aaa68f8425ed912904ef929c8f91987344d2ee8b908b1df1b0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlickrPhotoSearch::Application.config.session_store :cookie_store, key: '_flickr_photo_search_session'
