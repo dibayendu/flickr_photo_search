5c74c20d46cbe12fadc3ed51326257a167996ced
Dibayendu Dey <dibayendu.dey@gmail.com>
2014-08-24 15:46:24 +1000

    YAML file load needs to be successful in order to work

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module FlickrPhotoSearch
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      end if File.exists?(env_file) && YAML.load(File.open(env_file))
     end
     
     # configuring Flick
